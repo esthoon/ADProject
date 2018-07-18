e292e239a4d02d38aa95b929056dbe80279bb7cb
Avergrove <e0284007@u.nus.edu>
2018-07-18 14:04:42 +0800

    Added more dashboard charts and service - Added requisitionQuantityByDepartmentChart - Added purchaseQuantityByItemQuantityBarChart

--- Team3ADProject/Code/BusinessLogic.cs
+++ Team3ADProject/Code/BusinessLogic.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Team3ADProject.Model;
 
 namespace Team3ADProject.Code
 {
     public class BusinessLogic
     {
+        // Fetches a list of requisition orders from the database
+        public static List<requisition_order> GetRequisitionOrders()
+        {
+            var context = new LogicUniversityEntities();
+            var query = from x in context.requisition_order select x;
+            return query.ToList();
+        }
+
+        // Fetches a list of requisition orders from the database, filtered by most recent.
+        public static List<requisition_order> GetRecentRequisitionOrders(int length)
+        {
+            var context = new LogicUniversityEntities();
+            var query = from x in context.requisition_order orderby x.requisition_date select x;
+            query.OrderByDescending(x => x.requisition_date);
+
+            return query.Take(length).ToList();
+        }
+
+
     }
 }
