3b930b3278ee205cc1acb598629cd9b2684e4783
Esther <e0283990@u.nus.edu>
2018-07-24 16:07:02 +0800

    Added adjustment1

---
+++ Team3ADProject/Protected/AdjustmentForm1.aspx
@@ -0,0 +1,134 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdjustmentForm1.aspx.cs" Inherits="Team3ADProject.Protected.AdjustmentForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <div>
+                <asp:Label ID="Label2" runat="server" Text="Adjustment Form" Font-Bold="True" Font-Size="XX-Large"></asp:Label>
+            </div>
+            <div>
+                <table>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label4" runat="server" Text="Created on: "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelDate" runat="server" Text="date"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label6" runat="server" Text="Created by"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelName" runat="server" Text="name"></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <br />
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lbinum" runat="server" Text="Item no. "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelItemNum" runat="server" Text="Label"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lbdes" runat="server" Text="Description "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelItem" runat="server" Text="Label"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lbstk" runat="server" Text="Stock "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelStock" runat="server" Text="Label"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lbuprice" runat="server" Text="Unit Price "></asp:Label>
+                        </td>
+                        <td>
+                            $<asp:Label ID="LabelUnitPrice" runat="server" Text="Label"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style1">
+                            <asp:Label ID="lbad" runat="server" Text="Adjustment Quantity "></asp:Label>
+                        </td>
+                        <td class="auto-style1">
+                            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="TextBoxAdjustment_TextChanged">
+                                <asp:ListItem Selected="True">+</asp:ListItem>
+                                <asp:ListItem>-</asp:ListItem>
+                            </asp:DropDownList>
+                            <asp:TextBox ID="TextBoxAdjustment" runat="server" CausesValidation="True" AutoPostBack="true" OnTextChanged="TextBoxAdjustment_TextChanged"></asp:TextBox>
+                            <asp:CompareValidator ID="CompareValidator1" runat="server" ControlToValidate="TextBoxAdjustment" ErrorMessage="Quantity must be more than 0" ForeColor="Red" Operator="GreaterThan" Type="Integer" ValueToCompare="0"></asp:CompareValidator>
+                            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBoxAdjustment" ErrorMessage="No quantity stated" ForeColor="Red"></asp:RequiredFieldValidator>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="lbtcost" runat="server" Text="Total cost: "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:Label ID="LabelTotalCost" runat="server" Font-Bold="True"></asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="Label1" runat="server" Text="Remarks: "></asp:Label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="TextBoxRemarks" runat="server" Height="184px" Width="745px" TextMode="MultiLine"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <asp:Button ID="ButtonCancel" runat="server" Text="Cancel" Style="position: relative; float: right; top: 0px; margin-left: 0.5vw;" CssClass="btn btn-warning" CausesValidation="false" OnClientClick="javaScript:window.close(); return false;" />
+                            <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" Style="position: relative; float: right; top: 0px;" CssClass="btn btn-primary" OnClick="ButtonSubmit_Click" />
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </form>
+    <!--
+    <script>
+        $(document).ready(function () {
+            $('#DropDownList1, #TextBoxAdjustment').change(function(){
+                var a = parseFloat($('#<%=LabelUnitPrice.ClientID%>').text());
+                var b = $('#<%=DropDownList1.ClientID%>').val();
+                var c = $('#<%=TextBoxAdjustment.ClientID%>').val();
+                var d;
+                if (c != null && $.isNumeric(c)) {
+                    if (b == '-') {
+                        d = a * (-c);
+                    } else {
+                        d = a * c;
+                    }
+                    $('#<%=LabelTotalCost.ClientID%>').text(d);
+                }
+            })
+        })
+    </script>  
+-->
+</body>
+</html>
---
+++ Team3ADProject/Protected/AdjustmentForm1.aspx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Team3ADProject.Code;
+using Team3ADProject.Model;
+using System.Transactions;
+
+namespace Team3ADProject.Protected
+{
+    public partial class AdjustmentForm1 : System.Web.UI.Page
+    {
+        static employee user;
+        static inventory item;
+        static int headid, supid;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //retrieve user
+                if (Session["Employee"] != null)
+                {
+                    int employeeid = (int)Session["Employee"];
+                    user = BusinessLogic.GetEmployeeById(employeeid);
+                }
+                else
+                {
+                    //hardcoded
+                    Session["Employee"] = 10;
+                    user = BusinessLogic.GetEmployeeById(10);
+                    //redirect to login homepage
+                }
+                //retrieve headid
+                if (Session["Head_id"] != null)
+                {
+                    headid = (int)Session["Head_id"];
+                }
+                else
+                {
+                    headid = 12;
+                }
+                //retrieve headid
+                if (Session["Sup_id"] != null)
+                {
+                    supid = (int)Session["Head_id"];
+                }
+                else
+                {
+                    supid = 13;
+                }
+                LabelDate.Text = DateTime.Now.ToString("dd-MMM-yyyy");
+                LabelName.Text = user.employee_name;
+                UpdatePage();
+            }
+        }
+
+        // Updates page given a book
+        protected void UpdatePage()
+        {
+            // Grab the attributes from the URL
+            string param_itemcode = Request.QueryString["itemcode"];
+            if (param_itemcode != null)
+            {
+                string itemcode = param_itemcode;
+                item = BusinessLogic.GetInventory(itemcode);
+                LabelUnitPrice.Text = BusinessLogic.Adjprice(itemcode).ToString();
+                LabelStock.Text = item.current_quantity.ToString();
+                LabelItemNum.Text = item.item_number.ToString();
+                LabelItem.Text = item.description;
+
+            }
+
+        }
+
+        protected void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(ResolveUrl("~/Protected/ClerkInventory"));
+            //Response.Write("<script language='javascript'> { window.close();}</script>");
+        }
+
+        protected double TotalPrice()
+        {
+            int qty = 0;
+            if (TextBoxAdjustment.Text.Trim() != null && Int32.TryParse(TextBoxAdjustment.Text, out qty))
+            {
+                string param_itemcode = Request.QueryString["itemcode"];
+                double unitprice = BusinessLogic.Adjprice(param_itemcode);
+                string symbol = DropDownList1.SelectedItem.Value;
+                if (symbol == "-")
+                {
+                    return (unitprice * (-qty));
+                }
+                else
+                {
+                    return (unitprice * qty);
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        protected int ReturnQuantity()
+        {
+            int qty = 0;
+            if (TextBoxAdjustment.Text.Trim() != null && Int32.TryParse(TextBoxAdjustment.Text, out qty))
+            {
+                string symbol = DropDownList1.SelectedItem.Value;
+                if (symbol == "-")
+                {
+                    qty = (-qty);
+                }
+            }
+            return qty;
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            String today = DateTime.Now.ToString("yyyy-MM-dd");
+            int qty = Int32.Parse(TextBoxAdjustment.Text);
+            string email = RetrieveEmail(TotalPrice());
+            if (qty != 0)
+            {
+                try
+                {
+                    adjustment a = new adjustment()
+                    {
+                        adjustment_date = DateTime.ParseExact(today, "yyyy-MM-dd", null),
+                        employee_id = user.employee_id,
+                        item_number = item.item_number,
+                        adjustment_quantity = ReturnQuantity(),
+                        adjustment_price = TotalPrice(),
+                        adjustment_status = "Pending",
+                        employee_remark = TextBoxRemarks.Text,
+                        manager_remark = "",
+                    };
+
+                    try
+                    {
+                        using (TransactionScope tx = new TransactionScope())
+                        {
+                            BusinessLogic.CreateAdjustment(a);
+                            tx.Complete();
+                            Response.Write(BusinessLogic.MsgBox("Success: The adjustment request has been sent for approval"));
+                            BusinessLogic.sendMail("e0283990@u.nus.edu", "New Adjustment Request awaiting for approval", user.employee_name + " has submitted a new Adjustment Request for approval.");
+                        }
+                        //Response.Redirect("ClerkInventory.aspx");
+                        Response.Write("<script language='javascript'> { window.close();}</script>");
+                    }
+                    catch (System.Transactions.TransactionException ex)
+                    {
+                        Response.Write(BusinessLogic.MsgBox(ex.Message));
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write(BusinessLogic.MsgBox(ex.Message));
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    Response.Write(BusinessLogic.MsgBox(ex.Message));
+                }
+            }
+        }
+
+        protected void TextBoxAdjustment_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBoxAdjustment.Text.Trim() != null)
+            {
+                double price = TotalPrice();
+                LabelTotalCost.Text = "$" + price.ToString("0.00");
+            }
+        }
+
+        protected string RetrieveEmail(double price)
+        {
+            if (price > 250)
+            {
+                //return BusinessLogic.RetrieveEmailByEmployeeID(headid);
+                return "e0283390@u.nus.edu";
+            }
+            else
+            {
+                //return BusinessLogic.RetrieveEmailByEmployeeID(supid);
+                return "e0283390@u.nus.edu";
+            }
+        }
+    }
+}
