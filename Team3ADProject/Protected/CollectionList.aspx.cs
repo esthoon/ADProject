56ba685af6041eb8119581653e6f4ce911a0af4d
JoelFong85 <36076912+JoelFong85@users.noreply.github.com>
2018-07-25 10:45:29 +0800

    Joel finished validations

--- Team3ADProject/Protected/CollectionList.aspx.cs
+++ Team3ADProject/Protected/CollectionList.aspx.cs
@@ -27,14 +27,23 @@ namespace Team3ADProject.Protected
 
         protected void btn_submitCollectionList_Click(object sender, EventArgs e)
         {
+            //validate if textbox value is less than ordered & inventory
+
+            if (ValidatePreparedQty() < 0)
+            {
+                return;
+            }
+
+
+
             //(1) tracking qty collected per item for all dpts
             List<CollectionListItem> allDptCollectionList = new List<CollectionListItem>();
 
             foreach (GridViewRow gvr in gv_CollectionList.Rows)
             {
-                string itemNumber = gvr.Cells[0].Text; //if BoundField
+                string itemNumber = gvr.Cells[0].Text;
 
-                TextBox tb = (TextBox)gvr.FindControl("txt_QtyPrepared"); //if TemplateField
+                TextBox tb = (TextBox)gvr.FindControl("txt_QtyPrepared");
                 int qtyPrepped = Convert.ToInt32(tb.Text);
 
                 CollectionListItem c = new CollectionListItem(itemNumber, qtyPrepped);
@@ -112,9 +121,42 @@ namespace Team3ADProject.Protected
             Session["itemcode"] = itemcode;
             string url = "AdjustmentForm1.aspx?itemcode=" + itemcode;
             Response.Write("<script type='text/javascript'>window.open('" + url + "');</script>");
-            //Response.Redirect(url);
-
         }
 
+        protected int ValidatePreparedQty()
+        {
+            bool flag = false;
+            foreach (GridViewRow gvr in gv_CollectionList.Rows)
+            {
+                int qtyOrder = Convert.ToInt32(gvr.Cells[3].Text);
+                int qtyAvail = Convert.ToInt32(gvr.Cells[4].Text);
+
+                TextBox tb = (TextBox)gvr.FindControl("txt_QtyPrepared");
+                int qtyToPrep = Convert.ToInt32(tb.Text);
+
+                Label validator = (Label)gvr.FindControl("Label1");
+                validator.Visible = false;
+
+                if (qtyToPrep > qtyOrder)
+                {
+                    validator.Visible = true;
+                    validator.Text = "Amount is more than Ordered Qty";
+                    flag = true;
+                    break;
+                }
+                if (qtyToPrep > qtyAvail)
+                {
+                    validator.Visible = true;
+                    validator.Text = "Insufficient inventory";
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == true)
+                return -1;
+
+            else
+                return 1;
+        }
     }
 }
