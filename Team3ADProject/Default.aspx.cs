53ba7372ca7956afc68d8bf1aea5710a2d8cc3a3
Avergrove <e0284007@u.nus.edu>
2018-07-25 16:13:25 +0800

    Moved hardcoded session variables from global to default (onLogin) with variables that are fetched from database

--- Team3ADProject/Default.aspx.cs
+++ Team3ADProject/Default.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Team3ADProject.Model;
+using Team3ADProject.Code;
+using System.Web.Security;
 
 namespace Team3ADProject
 {
@@ -17,7 +20,14 @@ namespace Team3ADProject
         protected void Login1_LoggedIn(object sender, EventArgs e)
         {
             // Setup the session variables
-
+            Session["username"] = HttpContext.Current.User.Identity.Name;
+            employee emp = BusinessLogic.GetEmployeeByUserID((string)Session["username"]);
+            Session["Employee"] = emp.employee_id;
+            Session["Department"] = emp.department_id.Trim();
+            Session["role"] = Roles.GetRolesForUser(HttpContext.Current.User.Identity.Name).FirstOrDefault();
+            department dep = BusinessLogic.GetDepartmenthead(emp.department_id.Trim());
+            Session["Head_id"] = dep.head_id;
+            Session["supervisor_id"] = emp.supervisor_id;
 
             // Redirect users to their dashboard
             Response.Redirect(ResolveUrl("~/Protected/Dashboard"));
